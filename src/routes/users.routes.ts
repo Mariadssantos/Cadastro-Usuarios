6e643766838b9391c29fcc633865d457e5b58aa8
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-03-03 21:16:43 -0300

    Documentação swagger parte 1

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,7 +2,7 @@
 import { Router } from "express";
 
 import { createUserController } from "../modules/users/useCases/createUser";
-// import { listAllUsersController } from "../modules/users/useCases/listAllUsers";
+import { listAllUsersController } from "../modules/users/useCases/listAllUsers";
 import { showUserProfileController } from "../modules/users/useCases/showUserProfile";
 import { turnUserAdminController } from "../modules/users/useCases/turnUserAdmin";
 
@@ -20,8 +20,8 @@ usersRoutes.get("/:user_id", (request, response) =>
   showUserProfileController.handle(request, response)
 );
 
-// usersRoutes.get("/", (request, response) =>
-//   listAllUsersController.handle(request, response)
-// );
+usersRoutes.get("/", (request, response) =>
+  listAllUsersController.handle(request, response)
+);
 
 export { usersRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 /* eslint-disable */
+import swaggerUi from 'swagger-ui-express';
+import SwaggerFile from './swagger.json'
 import { app } from ".";
+
+app.use("/apiUser", swaggerUi.serve, swaggerUi.setup(SwaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
